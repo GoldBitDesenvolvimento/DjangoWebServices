63c1ccc2a10c994ef9af179388142ee55bfb0551
Gabriel Duarte <gabriellagoa10@yahoo.com.br>
2015-11-06 10:25:01 -0200

    Lingua do site modificada

--- source/source/settings.py
+++ source/source/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt_br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
