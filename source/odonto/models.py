e0f26bb9ae0ebf4e807999266355fca107154ded
gguilhermepires <gguilhermepires@gmail.com>
2015-11-08 11:53:47 -0200

    Adiconado os campos, falta criar aa ligações entre as tabelas.

--- source/odonto/models.py
+++ source/odonto/models.py
@@ -1,7 +1,64 @@
 from django.db import models
 
 # Create your models here.
+class endereco(models.Model):
+    rua = models.CharField(max_length=50)
+    bairro = models.CharField(max_length=50)
+    estado = models.CharField(max_length=50)
+    pais = models.CharField(max_length=50)
+    CEP = models.CharField(max_length=50)
+
+class pessoa(models.Model):
+    #usuario = models.CharField(max_length=50)
+    nome = models.CharField(max_length=50)
+    tiposangue = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=50)
+    rg = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=50)
+    celular = models.CharField(max_length=50)
+    dataNascimento = models.DateField()
+    models.ForeignKey(Publisher)
+    models.ManyToManyField(Author)
+
 class Usuario(models.Model):
 	usuario = models.CharField(max_length=50)
 	senha = models.CharField(max_length=50)
 	email = models.EmailField()
+
+# Create your models here.
+class Anamnese(models.Model):
+    usuario = models.CharField(max_length=50)
+    p01 = models.CharField(max_length=4)
+    p01b = models.CharField(max_length=255)
+    p02 = models.CharField(max_length=4)
+    p02b = models.CharField(max_length=255)
+    p03 = models.CharField(max_length=4)
+    p03b = models.CharField(max_length=255)
+    p04 = models.CharField(max_length=4)
+    p04b = models.CharField(max_length=255)
+    p05 = models.CharField(max_length=4)
+    p06 = models.CharField(max_length=4)
+    p07 = models.CharField(max_length=4)
+    p08 = models.CharField(max_length=4)
+    p09 = models.CharField(max_length=4)
+    p10 = models.CharField(max_length=4)
+    p11 = models.CharField(max_length=4)
+    p12 = models.CharField(max_length=4)
+    p13 = models.CharField(max_length=4)
+    p14 = models.CharField(max_length=4)
+    p14b = models.CharField(max_length=255)
+    p15 = models.CharField(max_length=4)
+    p16 = models.CharField(max_length=4)
+    p17 = models.CharField(max_length=4)
+    p18 = models.CharField(max_length=4)
+    p18b = models.CharField(max_length=255)
+    p19 = models.CharField(max_length=4)
+    p19b = models.CharField(max_length=255)
+    p20 = models.CharField(max_length=4)
+    p20b = models.CharField(max_length=255)
+    p21 = models.CharField(max_length=4)
+    p21b = models.CharField(max_length=255)
+    p22 = models.CharField(max_length=4)
+    p23 = models.CharField(max_length=4)
+    p23b = models.CharField(max_length=255)
+    
