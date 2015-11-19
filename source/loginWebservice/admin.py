b778f707a89d00ba53786ad5a8beb580be860fd3
gguilhermepires <gguilhermepires@gmail.com>
2015-11-19 16:20:59 -0200

    adicionado tabelas ao admin

--- source/loginWebservice/admin.py
+++ source/loginWebservice/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(SignUp)
+admin.site.register(User)
+admin.site.register(Person)
+
+admin.site.register(Endereco)
