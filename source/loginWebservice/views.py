904ec0d4abca6da29592b90c1461fa31e576b9d2
gguilhermepires <gguilhermepires@gmail.com>
2015-11-25 16:41:07 -0200

    Adicionado a funcao forgotPassword para enviar email

--- source/loginWebservice/views.py
+++ source/loginWebservice/views.py
@@ -25,7 +25,31 @@ def getLoginWebservice(request):
 		data = '[{"return": "invalid: Erro parameters on post"}]'	
 	
 	return HttpResponse(data)
-
+@csrf_exempt 
+def forgotPassword(request):	
+	if 'email' in request.POST:
+		user = User.objects.filter(email__contains = str(request.POST['email']))				
+		if not user:
+			data = '[{"return": "invalid:User do not exits"}]'		
+		else:
+			print 'Enviando para:'+user.get().email
+			html_message= """
+				<h1>Dados do Login</h1>
+				<h2>Login:"""+user.get().email+"""</h2>
+				<h2>Senha:"""+user.get().password+"""</h2>
+				<img src="http://www.goldbit.com.br/imagens/logo.png" height="100" width="400">
+			"""
+			send_mail("Lembrar senha",
+					"Senha para login:"+user.get().password,
+					"contato@goldbit.com.br",
+					[str(user.get().email)],
+					html_message=html_message,
+					fail_silently=False)
+			data = '{"return": "Password:Send"}'	
+	else:
+		data = '[{"return": "invalid:User do not exits"}]'		
+	
+	return HttpResponse(data)
 # Create your views here.
 @csrf_exempt 
 def getPaciente(request):
